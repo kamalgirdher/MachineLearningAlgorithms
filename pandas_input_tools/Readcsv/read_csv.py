b7b21cf238ae1ff6ccd9ac5e964c1c4e2aa87023
GIRDHER Kamal TGI/OLS <kamal.girdher@orange.com>
2019-02-18 14:27:26 +0530

    read csv example updated

--- pandas_input_tools/Readcsv/read_csv.py
+++ pandas_input_tools/Readcsv/read_csv.py
@@ -1,5 +1,6 @@
 #Code to read csv file using Pandasa
 
 import pandas as pd
-df=pd.read_csv('https://ed-public-download.app.cloud.gov/downloads/Most-Recent-Cohorts-Scorecard-Elements.csv')
+df=pd.read_csv('inp.csv')
+#df=pd.read_csv('https://ed-public-download.app.cloud.gov/downloads/Most-Recent-Cohorts-Scorecard-Elements.csv')
 print(df.head(5))
